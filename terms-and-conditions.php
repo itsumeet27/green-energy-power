bb1a43b31f4d0671343744e74c28dbbca35f080a
Sumeet Sharma <sksksharma0@gmail.com>
2020-05-23 21:46:17 +0530

    Terms and Conditions

---
+++ terms-and-conditions.php
@@ -0,0 +1,157 @@
+<?php include('includes/header.php'); ?>
+
+  <!-- Full Page Intro -->
+  <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/93.jpg'); background-repeat: no-repeat; background-size: cover;">
+
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+      <!-- Content -->
+      <div class="text-center white-text mx-5 wow fadeIn">
+        <h1 class="mb-4">
+          <strong>TERMS AND CONDITIONS</strong>
+          <hr class="my-4" style="width: 150px;border-color: #fff;border-width: 2px;border-radius:20px;border-style: groove;">
+        </h1>
+      </div>
+      <!-- Content -->
+
+    </div>
+    <!-- Mask & flexbox options-->
+  </div>
+  <section class="container py-5">
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Definition:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      For the purpose of addressing each party, hereby the customer who enrolls for the solar installation will be denoted as “Client” and the  Green Energy Power Organization (operated by Powerganic Foundation)  who does the installation and investment will be denoted as “Contractor” and the EPC (Engineering Procurement and construction) provider will be denoted as “Sub-contractor”.
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Discount:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      Free electricity or discounted electricity is based on the available load from solar panels and windmill any excess load is drawn from grid payable as local DISCOM tariff rates. 
+      <ul>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          <b>0% discount plan</b> – free electricity is subjected to the load produced from the solar panel or windmill which is mainly during the daytime for solar panels and throughout the day for windmill. The Power Purchase Agreement of 10 years applies in this case. The price is as per local DISCOM policy at the time of joining.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          <b>25% discount plan</b> – discount is subjected to the load produced from the solar panel which is mainly during the daytime, or windmill. If hybrid grid is required for complete discounted electricity the battery installation price related adjustment on the period of free or discounted electricity will be adjusted accordingly
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          <b>50% discount plan</b> – discounts is subjected to the load produced from the solar panel which is mainly during the daytime.
+        </li>
+      </ul>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Panel installation:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          We have various Sub-contractors who are experienced (minimum 5years) EPC in the solar panel installation business will visit your enrolled address site with prior intimation through phone or email and approval from you to enter your premises for assessment of installation.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          The Sub-contractors shall report directly to Client or Contractor if extra requirements need to be attended. Upon such case, Client will be notified, and Contractor will discuss with Client for extra cost impact. Contractor will not adjust any extra requirements that are cost impact without the permission of Client. (Though in RESCO the investment is borne by Contractor but the extra fittings as per schemes may need adjustment for the time period of free electricity in the 0% and 25% discount such as 10 year will be extended to 11 or 12 years for cost adjustment).
+        </li>
+      </ol>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Access:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Whenever required from the installation to the end of life of panel the Client agrees to grant permission to sub-contractors who are employed and contracted by Contractor for carrying out the jobs by prior permission from Client before such access to Clients nominated premises for installation and follow up maintenance works.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          In the event of any damage occurred to the premises or property during the installation or maintenance and that is directly caused due to the impact of the installation or maintenance works will be assessed and fixed by Contractor.  
+        </li>
+      </ol>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Damage:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Any damage that is caused due to natural calamity to the units or equipment installed will be insured by the Contractor, but such damages should be pronounced government natural disaster. Any other disaster will be analyzed by Contractor and Sub-contractor for its credibility.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Client agrees that, if the damage caused by Client negligence or any inducive manner will be assessed and the repair or replacement or damage expenses are liable to Client.
+        </li>
+      </ol>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Injury & Accident:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          In case of any assigned Contractor or Subcontractor personnel’s behavior is unacceptable, Client in his / her own discretion can deny access to such person and Client agrees to immediately report the incident to Contractor.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Any injury that happens to the personnel whilst working at the premises is a subject matter of the Contractor & Sub-contractor, if possible, Client reporting the incident will be appreciated. 
+        </li>
+      </ol>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Equipment and Device:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Client agrees with the ownership of all the equipment to remain until its life with the Contractor, the Client agrees to allot the space that is occupiable by the devices and equipment, which is normally the rooftop or terrace or garden and the space for the battery inside their home or enclosed space if opted.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Client also agrees that after installation in the agreed best suitable space, it is immovable or alterable except for any reasonable causes.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          In the case of change of place or alteration upon Clients reasonable causes, all cost bearable by Client.
+        </li>
+      </ol>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Subsidy:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          The Client agrees that the Contractor will claim all available government subsidy in relation to the installment of solar panels and windmill or any renewable energy equipment in their premises.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          As a not for profit company Contractor pledges the subsidy being used for the rural area farmland independent energy generation schemes.
+        </li>
+      </ol>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Billing and payment:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          The Client agrees for Contractor in billing and handling of their electricity connection on behalf of them for the purpose of applying the discount plans. Therefore, the client will not be able to receive any credits or any other benefits from DISCOM for the net metering consumption calculation or reading.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Client agrees to pay their bill to the Contractor as per the bill adjustment calculated based on chosen discount plans or scheme.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Client agrees for Contractor to claim all the Feed In Tariff (FIT) from the DISCOM as per local DISCOM policy. (The unused units electricity from the green energy equipment goes to grid such units will be available for FIT from DISCOM).
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Client agrees that the discount only applies to the generated electricity consumed from the green energy equipment installed.
+        </li>
+      </ol>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Ownership:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      Client agrees that the ownership for the equipment installed in relation with the green energy or renewable energy will stay with the Contractor until its plan period chosen by Client. The following are the plans and ownership period mentioned during the enrollment;
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">0% discount plan for 10years and ownership is transferable to Client after the 10years discount period</li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">25% discount plan for 15 years and ownership is transferable to Client after 15 years discount period</li>
+      </ol>
+    </p>
+    <h4 class="font-weight-bold h4-responsive" style="color:#555">Premises space allocation for the equipment:</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <ol type="1">
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          The space allocated by the Client in no means will be considered as lease or rental or exchange by Contractor. (It is comparable as a dish antenna equipment installation service where the equipment stays as a property of the supplier though it is sold to the customer).
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Client is responsible for the authorization of the premises space allocation for green energy equipment in being the legal owner of the property.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          In the event of Client selling his / her property, it is the obligation of the Client to inform 1 month (calendar days) prior to the Contractor. In this case Contractor may approach to the new owner of the property for transformation of this arrangement and contract. Whatever the case may be, existing Contract terms and conditions will continue to prevail to the new owner.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          If a lessee of the premises interested in the installation can represent with valid authorization document from lessor who is the legal owner of the property or premises. 
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Client agrees that by agreeing to this terms and conditions, it is their responsibility to ensure that he / she is having legal authority to do so.
+        </li>
+      </ol>
+    </p>
+  </section>
+
+<?php include('includes/footer.php'); ?>  
