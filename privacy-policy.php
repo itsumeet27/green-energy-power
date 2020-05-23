d56a5256fcac9281bc14532b3cbc847ee3c53ac3
Sumeet Sharma <sksksharma0@gmail.com>
2020-05-23 21:58:53 +0530

    Privacy Policy

---
+++ privacy-policy.php
@@ -0,0 +1,93 @@
+<?php include('includes/header.php'); ?>
+
+  <!-- Full Page Intro -->
+  <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/93.jpg'); background-repeat: no-repeat; background-size: cover;">
+
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+      <!-- Content -->
+      <div class="text-center white-text mx-5 wow fadeIn">
+        <h1 class="mb-4">
+          <strong>PRIVACY POLICY</strong>
+          <hr class="my-4" style="width: 100px;border-color: #fff;border-width: 2px;border-radius:20px;border-style: groove;">
+        </h1>
+      </div>
+      <!-- Content -->
+
+    </div>
+    <!-- Mask & flexbox options-->
+
+  </div>
+  <!-- Full Page Intro -->
+  <section class="container py-5">
+  	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+  		This privacy policy (“policy”) will help you understand how Green Energy Power Organisation  (“GEPO”, “us”, “we”, “our”) uses and protects the data you provide to us when you visit and use <a href="https://www.greenenergypower.org">www.greenenergypower.org</a> (“website”, “service”).
+  	</p>
+  	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+  		We reserve the right to change this policy at any given time, of which you will be promptly updated. If you want to make sure that you are up to date with the latest changes, we advise you to frequently visit this page.
+  	</p>
+    <h4 class="h4-responsive font-weight-bold" style="color: #555">What User Data We Collect</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      When you visit the website, we may collect the following data:
+      <ul>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Your IP address.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Your contact information and email address.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          Your electricity connection details.
+        </li>
+      </ul>
+    </p>
+    <h4 class="h4-responsive font-weight-bold" style="color: #555">Why We Collect Your Data</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      We are collecting your data for following reasons:
+      <ul>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          To better understand your needs.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          To improve our services and products.
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          To connect with DISCOM for fetching the details of your electricity connection
+        </li>
+        <li class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+          To contact you to fill out surveys and participate in other types of market research.
+        </li>
+      </ul>
+    </p>
+    <h4 class="h4-responsive font-weight-bold" style="color: #555">Safeguarding and Securing the Data</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      GEPO is committed to securing your data and keeping it confidential. We have done all in our power to prevent data theft, unauthorized access, and disclosure by implementing the latest technologies and software, which help us safeguard all the information we collect online.
+    </p>
+    <h4 class="h4-responsive font-weight-bold" style="color: #555">Our Cookie Policy</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      When you submit your registration, you agree to our cookies, which are to customize our website to your needs. After we use the data for statistical analysis, the data is completely removed from our systems. Our cookies do not collect your online behavior or any other data related to it.
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      Please note that cookies do not allow us to gain control of your computer in any way. They are strictly used to improve our website.
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      If you want to disable cookies, you can do it by accessing the settings of your internet browser.
+    </p>
+    <h4 class="h4-responsive font-weight-bold" style="color: #555">Links to Other Websites</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      Our website contains links that lead to other website. If you click on these links GEPO is not held responsible for your data and privacy protection. Visiting those websites is not governed by this privacy policy agreement. Make sure to read the privacy policy documentation of the website you go to from our website.
+    </p>
+    <h4 class="h4-responsive font-weight-bold" style="color: #555">Restricting the Collection of your Personal Data</h4>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      At some point, you might wish to restrict the use and collection of your personal data. You can achieve this by doing the following:
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      If you have already agreed to share your information with us, feel free to contact us via email and we will be more than happy to change this for you.
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      GEPO will not lease, sell or distribute your personal information to any third parties, unless we have your permission. We might do so if the law forces us or law enforcement body requires it.
+    </p>
+  </section>
+
+<?php include('includes/footer.php'); ?>
