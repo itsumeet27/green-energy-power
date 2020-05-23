046722910deb25676d328a54ea2981e19b521871
Sumeet Sharma <sksksharma0@gmail.com>
2020-05-23 20:58:21 +0530

    Why Us Page

---
+++ why-us.php
@@ -0,0 +1,39 @@
+<?php include('includes/header.php'); ?>
+
+  <!-- Full Page Intro -->
+	  <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/93.jpg'); background-repeat: no-repeat; background-size: cover;">
+
+	    <!-- Mask & flexbox options-->
+	    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+	      <!-- Content -->
+	      <div class="text-center white-text mx-5 wow fadeIn">
+	        <h1 class="mb-4">
+	          <strong>WHY US?</strong>
+	          <hr class="my-4" style="width: 75px;border-color: #fff;border-width: 2px;border-radius:20px;border-style: groove;">
+	        </h1>
+	      </div>
+	      <!-- Content -->
+
+	    </div>
+	    <!-- Mask & flexbox options-->
+
+	  </div>
+  <!-- Full Page Intro -->
+
+  <section class="container py-5">
+  	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+  		We operate under Powerganic Foundation which is a registered Section 8 company (Not for Profit company) behind a mission that will enable a sustainable rural world with our projects of Zero Emission lifestyle with renewable or green energy power, organic farming, rural digital education on governance, skill development center for organic farming and upliftment of underprivileged farmers.
+  	</p>
+  	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+		We realized that for any element to remain sustainable, it should be kept away from commercial vested interests. Only then can a sustainable process be created. Visit <a href="https://www.powerganic.org" target="_blank">www.powerganic.org</a> for more information.
+	</p>
+	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+		Indians have a heritage that deeply links with the lifestyle of Zero Carbon Emission and organic-based agriculture practices in our ancient times. Therefore, we intend to cut the commercial vested interests to promote the sustainable process of creating farmlands with Emission Free Independent Energy and Organic Farming through the funding/subsidy allotted in bringing rooftop solar or windmill projects that are the schemes brought by World Bank Initiative for climate change action, Central Government of India for renewable energy and various State Governments for green energy initiative.
+	</p>
+	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+		We will be your drivers for climate change action and in creating a sustainable life.
+  	</p>
+  </section>
+
+<?php include('includes/footer.php'); ?>
