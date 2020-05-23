18b9b880c0e339bb1f8b3b2356c2e3d1057498d6
Sumeet Sharma <sksksharma0@gmail.com>
2020-05-23 20:51:57 +0530

    How it Works Page

--- about-us.php
+++ about-us.php
@@ -1,6 +1,6 @@
 <?php include('includes/header.php'); ?>
 
-<!-- Full Page Intro -->
+  <!-- Full Page Intro -->
   <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/93.jpg'); background-repeat: no-repeat; background-size: cover;">
 
     <!-- Mask & flexbox options-->
@@ -22,11 +22,11 @@
   <!-- Full Page Intro -->
   <section class="container py-5">
   	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
-		We are a group of young Engineers, Agriculturists and Business entrepreneurs who have lived and travelled across the planet working in various fields with wide knowledge. In our due course of life, we realized the need for a life balanced with sustainability which starts from removing the commercial interests behind basic and essential commodities, and critical subjects. Therefore, we joined hands to combat climate change along with the high industrial chemical-based fertilizer food production.
-	</p>
-	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
-		We are based on a village in Warangal. We all come from the roots of farming families. Our mission is to help the farming community with basic free electricity with the funds that we receive in the subsidy provided from the Government for renewable energy and the profit we make through this venture. The Green Energy Power Organisation is a trade name that is administered and operated by Powerganic Foundation. (For more information about Powerganic Foundation click here)
-	</p>
+  		We are a group of young Engineers, Agriculturists and Business entrepreneurs who have lived and travelled across the planet working in various fields with wide knowledge. In our due course of life, we realized the need for a life balanced with sustainability which starts from removing the commercial interests behind basic and essential commodities, and critical subjects. Therefore, we joined hands to combat climate change along with the high industrial chemical-based fertilizer food production.
+  	</p>
+  	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+  		We are based on a village in Warangal. We all come from the roots of farming families. Our mission is to help the farming community with basic free electricity with the funds that we receive in the subsidy provided from the Government for renewable energy and the profit we make through this venture. The Green Energy Power Organisation is a trade name that is administered and operated by Powerganic Foundation. (For more information about Powerganic Foundation click here)
+  	</p>
   </section>
 
 <?php include('includes/footer.php'); ?>
---
+++ how-it-works.php
@@ -0,0 +1,69 @@
+<?php include('includes/header.php'); ?>
+
+  <!-- Full Page Intro -->
+  <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/93.jpg'); background-repeat: no-repeat; background-size: cover;">
+
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+      <!-- Content -->
+      <div class="text-center white-text mx-5 wow fadeIn">
+        <h1 class="mb-4">
+          <strong>HOW IT WORKS?</strong>
+          <hr class="my-4" style="width: 100px;border-color: #fff;border-width: 2px;border-radius:20px;border-style: groove;">
+        </h1>
+      </div>
+      <!-- Content -->
+
+    </div>
+    <!-- Mask & flexbox options-->
+
+  </div>
+  <!-- Full Page Intro -->
+  <section class="container py-5">
+  	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+  		Our project is based on the RESCO model. Your investment is <b>Zero Rupees</b>. 
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      A RESCO is a Renewable Energy Service Company which installs the renewable energy equipment and monetizes the energy produced from it to cover investment and operation cost.
+  	</p>
+  	<p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+  		You are allocating your roof or any space for the solar panels or windmill when you choose a roof top solar or windmill plan. We charge you for the electricity you use for a said period of time or its lifetime with <b>your choice of discount plan</b>. Depending on the scheme or plan you choose, the equipment ownership will be transferred to you or continue to remain ours while you enjoy the benefits of <b>discounted electricity price</b>.
+  	</p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      There are conditions, which are a minimum requirement of daytime usage of electricity. For the industrial or commercial sector the minimum requirement is 1200 units per month and for the Residential sector, a min of 400 units per month after the first 200 units. If your minimum usage is not within the limits, you can club with your neighbors who are located not more than 100 m radius from your choice of premises to install the green energy equipment.
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      The discounts are calculated based on average slab price. For example, if you have consumed 600 units, then 201 to 400 is priced at ₹4.50, 401 to 500 at ₹5.50 and 501 to 600 at ₹7. Therefore, 50% discount is applied on the average of ₹4.50, ₹5.50 and ₹7 which will be ₹2.85.
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      Moreover, the 50% discount scheme is not available in every state DISOM, depending upon your usage data and your state or local DISCOM your eligibility for the scheme will be notified to you.
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      The panels produce electricity during the daytime only, therefore, a minimum requirement to calculate the possible revenue generation for investment repayment is necessary.
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      Currently the windmill is only available for 0% discount plan or scheme, but we are striving hard to bring the windmill soon by Third Quarter of 2020 for all plans or schemes that will enable electricity generation in nighttime. When available, we will install the windmill, if preferable, which will undergo discount period adjustments.
+    </p>
+    <h3 class="h3-responsive font-weight-bold" style="color: #555">HOW TO ENROLL?</h3>
+    <p class="text-justify mt-3" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <b>Step 1</b> – Enroll by providing electricity DISCOM details (allowing us to handle payments and bill generation accordingly to discount plans) and create account
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <b>Step 2</b> – Solar panel installer will call you to book appointment to visit your place for assessment
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <b>Step 3</b> – If your place has good sunlight, installer will book appointment for the day of installation with you on the day of visit to your place
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <b>Step 4</b> – Installation and connection to grid with net metering
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <b>Step 5</b> – Day 1 of supply start will be witnessed and reported to you (Client).
+    </p>
+    <p class="text-justify" style="font-weight: 400;color: #555;line-height: 1.7em;font-size: 17px;">
+      <b>Step 6</b> – First Bill with discount applied will be delivered and cleared
+    </p>
+  </section>
+
+<?php include('includes/footer.php'); ?>
