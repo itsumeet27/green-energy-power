85f63022c1d85247999c54204f26e7844bb2629e
Sumeet Sharma <sksksharma0@gmail.com>
2020-05-24 13:19:43 +0530

    Enrollment Form

---
+++ enrollment.php
@@ -0,0 +1,17 @@
+<?php include('includes/header.php'); ?>
+
+  <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/93.jpg'); background-repeat: no-repeat; background-size: cover;">
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+      <div class="text-center white-text mx-5 wow fadeIn">
+        <h1 class="mb-4">
+          <strong>ENROLLMENT FORM</strong>
+          <hr class="my-4" style="width: 125px;border-color: #fff;border-width: 2px;border-radius:20px;border-style: groove;">
+        </h1>
+      </div>
+    </div>
+  </div>
+  <section class="container py-5">
+
+  </section>
+
+<?php include('includes/footer.php'); ?>
