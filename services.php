6eca7823a7a8f10618a44690d23574247b99362a
Sumeet Sharma <sksksharma0@gmail.com>
2020-05-24 13:06:40 +0530

    Services

---
+++ services.php
@@ -0,0 +1,93 @@
+<?php include('includes/header.php'); ?>
+
+  <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/93.jpg'); background-repeat: no-repeat; background-size: cover;">
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+      <div class="text-center white-text mx-5 wow fadeIn">
+        <h1 class="mb-4">
+          <strong>SERVICES</strong>
+          <hr class="my-4" style="width: 80px;border-color: #fff;border-width: 2px;border-radius:20px;border-style: groove;">
+        </h1>
+      </div>
+    </div>
+  </div>
+  
+  <section class="container py-5">
+    <div id="roof-top-solar" class="mt-4">
+      <h2 class="h2-responsive text-center pb-4 font-weight-bold" style="color: #555">ROOF TOP SOLAR</h2>
+      <div class="row" style="padding: 2em 0 4em 0">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <img src="img/roof-top-solar.jpg" style="width: 100%;height: 100%">
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <!-- Homes -->
+          <h4 class="h4-responsive text-justify font-weight-bold" style="color: #555">Homes</h4>
+          <ul class="text-justify">
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              0% discount on your electricity bills for 10 years then free electricity until lifetime of the panel with battery back-up – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              25% discount on your electricity bills for 15 years then free electricity until lifetime of the panel – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              50% discount on your electricity bills until the lifetime of the panels – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+          </ul>
+          <!-- Industry or sheds, cold storage, commercial building, institution buildings -->
+          <h4 class="h4-responsive text-justify font-weight-bold" style="color: #555;line-height: 1.7em">Industry or sheds, cold storage, commercial building, institution buildings</h4>
+          <ul class="text-justify">
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              0% discount on your electricity bills for 10 years then free electricity until lifetime of the panel with battery back-up – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              25% discount on your electricity bills for 15 years then free electricity until lifetime of the panel – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              50% discount on your electricity bills until the lifetime of the panels – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div id="vertical-windmill">
+      <h2 class="h2-responsive text-center p-2 font-weight-bold" style="color: #555">VERTICAL WINDMILL</h2>
+      <div class="row" style="padding: 2em 0">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <img src="img/vertical-windmill.jpg" style="width: 100%;">
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <!-- High raised buildings -->
+          <h4 class="h4-responsive text-justify font-weight-bold" style="color: #555">High raised buildings</h4>
+          <ul class="text-justify">
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              25% Discount on your electricity bills plan – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              50% Discount on your electricity bills plan – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+          </ul>
+          <!-- Farmlands / farmhouse -->
+          <h4 class="h4-responsive text-justify font-weight-bold" style="color: #555">Farmlands/Farmhouse</h4>
+          <ul class="text-justify">
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              25% Discount on your electricity bills plan – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              50% Discount on your electricity bills plan – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+          </ul>
+          <!-- Coastal areas -->
+          <h4 class="h4-responsive text-justify font-weight-bold" style="color: #555">Coastal areas</h4>
+          <ul class="text-justify">
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              25% Discount on your electricity bills plan – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+            <li class="text-justify" style="font-size: 17px;line-height: 1.7em;color: #555;font-weight: 400">
+              50% Discount on your electricity bills plan – <a href="enrollment.php">Click here to enrol</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php include('includes/footer.php'); ?>
