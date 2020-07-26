b6f0407192d537bbe163a1b8d320cbf274ef5124
Sumeet Sharma <sksksharma0@gmail.com>
2020-07-26 12:26:37 +0530

    Login and Forgot Password

--- forgot-password.php
+++ forgot-password.php
@@ -27,7 +27,7 @@
 			<div class="col-md-4">
 				<div class="card" style="background-color: rgba(255,255,255,0.9);">
 					<h5 class="card-header elegant-color white-text text-center py-4">
-				    	<strong>Log in</strong>
+				    	<strong>Change Password</strong>
 				  	</h5>
 					<form action="" method="post" class="needs-validation" novalidate>
 						<div class="card-body">
@@ -39,51 +39,20 @@
 								<input type="password" id="password" class="form-control form-control-sm" name="password"  is-valid is-invalid required>
 								<label for="password">Password</label>
 							</div>
-							<div class="text-center">
-								<p class="p-2">Forgot your password? <a href="forgot-password.php">Click here</a></p>
+							<div class="md-form form-sm">
+								<input type="password" id="confirm_password" name="confirm_password" class="form-control mb-4" is-valid is-invalid required>
+								<label for="password">Confirm Password</label>
 							</div>
 						</div>			
 						<div class="card-footer">
 							<div class="float-right">
-								<button type="submit" name="login" class="btn btn btn-elegant mb-3">Login</button>
+								<button type="submit" name="submit" class="btn btn btn-elegant mb-3">Submit</button>
 							</div>
 						</div>
 					</form>
-					<?php 
-						if(isset($_POST['login'])){
-							$email = $_POST['email'];
-							$password = md5($_POST['password']);
-							$query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
-							$result = $db->query($query);
-							$num = mysqli_fetch_array($result);
-							$status = $num['status'];
-							if($num > 0){	
-								if($status == 0){
-									echo "<script>alert('Verify your Email Id by clicking  the link In your mailbox')</script>";
-								}else{
-									$_SESSION['email']=$email;
-									$_SESSION['id']=$num['id'];
-									$_SESSION['firstname']=$num['firstname'];
-									$extra="profile/index.php";
-									$host=$_SERVER['HTTP_HOST'];
-									$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-									echo "<script>window.open('profile/index.php','_self')</script>";
-									exit();
-								}
-							}else{
-								echo "<script>alert('Invalid login credentials, please try again!')</script>";
-								$extra="login.php";
-								$host  = $_SERVER['HTTP_HOST'];
-								$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-								echo "<script>window.open('login.php','_self')</script>";
-								exit();
-							}
-						}
-
-					?>
 				</div>
 			</div>
-			<div class="col-md-4"></div>
 		</div>
 	</div>
+
 <?php include('includes/footer.php'); ?>
